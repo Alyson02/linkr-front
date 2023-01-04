732a2c43681cf7153d3bc3bc2cc81ff9b8b1a838
Alyson02 <alyson.subrim2002@gmail.com>
2023-01-04 01:59:09 -0300

    feat: add-post

---
+++ src/components/pageTitle.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const PageTitle = styled.h1`
+  font-family: "Oswald";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 43px;
+  line-height: 64px;
+  color: #ffffff;
+`;
+
+export default PageTitle;
