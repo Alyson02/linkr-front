1ba08746fdaf10587963695445ea19409222ff16
RodrigoYujiKogaKuroda <yujikuroda.rykk@gmail.com>
2023-01-06 17:11:29 -0300

    feat: Implemented login page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./components/GlobalStyle";
+import SignIn from "./pages/SignIn";
 import TimeLine from "./pages/Timeline";
 import UserPosts from "./pages/UserPosts";
 
@@ -8,6 +9,7 @@ export default function App() {
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
+        <Route element={<SignIn />} path="/" />
         <Route element={<TimeLine />} path="/timeline" />
         <Route element={<UserPosts />} path='/user/:id' />
       </Routes>
