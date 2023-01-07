55e37f141baa86dacfaa2b35a511d124e12b73c8
matheuslimaOS <souza_matheus@discente.ufg.br>
2023-01-07 07:25:03 -0300

    fix: modal of deletion

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import GlobalStyle from "./components/GlobalStyle";
 import SignIn from "./pages/SignIn";
 import TimeLine from "./pages/Timeline";
 import UserPosts from "./pages/UserPosts";
+import styled from "styled-components";
+import { BaseModalBackground } from "styled-react-modal";
+import { ModalProvider } from "styled-react-modal";
+
+const ModalBackground = styled(BaseModalBackground)`
+  background: rgba(255, 255, 255, 0.8);
+`
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Routes>
-        <Route element={<SignIn />} path="/" />
-        <Route element={<TimeLine />} path="/timeline" />
-        <Route element={<UserPosts />} path='/user/:id' />
-      </Routes>
-    </BrowserRouter>
+    <ModalProvider backgroundComponent={ModalBackground}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Routes>
+          <Route element={<SignIn />} path="/" />
+          <Route element={<TimeLine />} path="/timeline" />
+          <Route element={<UserPosts />} path='/user/:id' />
+        </Routes>
+      </BrowserRouter>
+    </ModalProvider>
   );
 }
